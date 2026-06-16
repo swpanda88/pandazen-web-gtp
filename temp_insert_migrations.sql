42ff2ca3407ebbd0133fc4a5fb0d36b63769b3e5
Sev Hass <sev@systemfix.co.uk>
2026-06-16 22:29:22 +0100

    chore: remove accidental scratch files from public form fix

--- temp_insert_migrations.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO d1_migrations (name) VALUES ('0001_cleanops_v1_schema.sql');
-INSERT INTO d1_migrations (name) VALUES ('0002_cleanops_v1_seed_dev.sql');
-INSERT INTO d1_migrations (name) VALUES ('0003_cleanops_v1_seed_cleanup.sql');
-INSERT INTO d1_migrations (name) VALUES ('0004_cleanops_requests_ui_fields.sql');
